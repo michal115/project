359f4ccfc5bc4bdf6e3b4705b4e2ccd6bcdbdc16
michal115 <35937494+michal115@users.noreply.github.com>
2018-01-31 00:17:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "event.h"
+
 using namespace std;
 
 int main()
 {
-    Event w1;
+    
 
-    cout<<"Dokonalem pierwsza zmiane"<<endl;
-    w1.show();
+    cout<<"Moj program na GitHub"<<endl;
+  
 
     return 0;
 }
